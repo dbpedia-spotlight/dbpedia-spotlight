4d2f9d938346146e56a041e9fbeadba4eb6333fc
Max Jakob <max.jakob@gmail.com>
2011-02-08 16:51:13 +0000

    added license header to all Java files; for Scala files, they are still missing!!!

--- src/main/java/org/dbpedia/spotlight/lucene/search/TermScorer.java
+++ src/main/java/org/dbpedia/spotlight/lucene/search/TermScorer.java
@@ -1,17 +1,11 @@
 /**
- * COPIED HERE SINCE LUCENE DID NOT MAKE IT PUBLIC, SO IT CANNOT BE CALLED FROM OUTSIDE THE PACKAGE. :(
- */
-package org.dbpedia.spotlight.lucene.search;
-
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2011 Pablo Mendes, Max Jakob
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,6 +13,11 @@ package org.dbpedia.spotlight.lucene.search;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * COPIED HERE SINCE LUCENE DID NOT MAKE IT PUBLIC, SO IT CANNOT BE CALLED FROM OUTSIDE THE PACKAGE. :(
+ */
+package org.dbpedia.spotlight.lucene.search;
 
 import java.io.IOException;
 
--- src/web/rest/src/java/org/dbpedia/spotlight/web/rest/Configuration.java
+++ src/web/rest/src/java/org/dbpedia/spotlight/web/rest/Configuration.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2011 Pablo Mendes, Max Jakob
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -12,7 +28,7 @@ package org.dbpedia.spotlight.web.rest;
 public class Configuration {
 
         //TODO make this configurable
-    final static String spotterFile    = "spotterDictionary";
-    final static String indexDirectory = "index";
+    final static String spotterFile    = "e:\\dbpa\\data\\__smallSpotDictList.spotterDictionary";
+    final static String indexDirectory = "e:\\dbpa\\data\\index\\DisambigIndex.restrictedSFs.plusTypes-plusSFs\\";
 
 }
