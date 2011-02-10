6b3b331dc8534b7acb2770a2983174db917f3596
Pablo Mendes <pablomendes@gmail.com>
2011-02-10 13:45:29 +0000

    installation script for linux

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,4 @@
 mvn install:install-file -Dfile=lib/nxparser-1.1.jar -Dpackaging=jar -DgroupId=org.semanticweb.yars -DartifactId=nx.parser -Dversion=1.1
-
-mvn install:install-file -Dfile=lib/cloud9.jar -Dpackaging=jar -DgroupId=edu.umd -DartifactId=cloud9 -Dversion=SNAPSHOT
-
 mvn install:install-file -Dfile=lib/lingpipe-4.0.0.jar -Dpackaging=jar -DgroupId=com.aliasi -DartifactId=lingpipe -Dversion=4.0.0
-
+mvn install:install-file -Dfile=lib/cloud9.jar -Dpackaging=jar -DgroupId=edu.umd -DartifactId=cloud9 -Dversion=SNAPSHOT
+mvn install
