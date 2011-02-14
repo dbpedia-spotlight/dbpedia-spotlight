fed31bf26922b3c9390d440a0442bd05aed8e789
Max Jakob <max.jakob@gmail.com>
2011-02-14 18:29:20 +0000

    optimized import to get rid of unnecessary dependency

--- src/main/java/org/dbpedia/spotlight/io/DataLoader.java
+++ src/main/java/org/dbpedia/spotlight/io/DataLoader.java
@@ -16,12 +16,13 @@
 
 package org.dbpedia.spotlight.io;
 
-import com.sun.org.apache.xerces.internal.xs.StringList;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.io.*;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
