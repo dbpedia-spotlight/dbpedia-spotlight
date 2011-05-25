fa51a6cb428b1571b3fe7eaac55c5865a4132e89
Joachim Daiber <jodaiber@gmx.de>
2011-05-25 18:48:52 +0000

    adds TaggedText

---
+++ core/src/main/scala/org/dbpedia/spotlight/model/TaggedText.scala
@@ -0,0 +1,18 @@
+package org.dbpedia.spotlight.model
+
+import org.dbpedia.spotlight.tagging.TaggedTokenProvider
+
+/**
+ * @author Joachim Daiber
+ */
+
+class TaggedText(text : String, var taggedTokenProvider : TaggedTokenProvider) extends Text(text) {
+
+  //Initialize the Tagged Text by pos-tagging it with the current TaggedTokenProvider
+  taggedTokenProvider.initialize(text)
+
+  override def equals(that : Text) =
+  {
+    text.equals(that.text)
+  }
+}
