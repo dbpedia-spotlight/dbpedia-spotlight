a6134110d976bb30bdfaddaadea024ab2f3bfe82
Pablo Mendes <pablomendes@gmail.com>
2011-07-27 12:59:28 +0000

    changing indexing properties and index.sh for ease to run

--- bin/index.sh
+++ bin/index.sh
@@ -1,10 +1,16 @@
+# You are expected to run this script from inside the bin directory in your DBpedia Spotlight installation
+# Adjust the paths here if you don't.
+# @author maxjakob, pablomendes
+
+here = `pwd`
+
 INDEX_CONFIG_FILE=../conf/index.properties
 
 HEAPSPACE=2g
 
-
 mkdir ../data
 
+cd ../core
 mvn scala:run -DmainClass=org.dbpedia.spotlight.util.SurrogatesUtil -DjvmArg=-Xmx$HEAPSPACE "-DaddArgs=$INDEX_CONFIG_FILE"
 
 mvn scala:run -DmainClass=org.dbpedia.spotlight.util.SaveWikipediaDump2Occs -DjvmArg=-Xmx$HEAPSPACE "-DaddArgs=$INDEX_CONFIG_FILE|../data/occs.tsv"
