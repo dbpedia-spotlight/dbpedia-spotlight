8613bb6d96bfe29f8f7e77cab7170c0f77026275
Pablo Mendes <pablomendes@gmail.com>
2011-08-26 14:35:17 +0000

    added suggest, subst click event for button, removed global inputText var

--- demo/dbpedia-spotlight-0.3.js
+++ demo/dbpedia-spotlight-0.3.js
@@ -24,6 +24,66 @@
       'spotter': 'LingPipeSpotter' // one of: LingPipeSpotter,AtLeastOneNounSelector,CoOccurrenceBasedSelector
     };
 
+   var Parser = {
+	getSelectBox: function(resources, className) {
+             var snippet =  "<ul class='"+className+"s'>";
+             //console.log(resources);
+             var options = ""; $.each(resources, function(i, r) { 
+                 options += "<li class='"+className+"-" + i + "'><a href='" + r["@uri"] + "' about='" + r["@uri"] + "'>" + r["@label"] + "</a>";
+                 //TODO settings.showScores = ["finalScore"] foreach showscores, add k=v
+                 if (settings.showScores == 'yes') options += " <span>(" + parseFloat(r["@finalScore"]).toPrecision(3) +")</span>";
+                 options += "</li>"; 
+             });
+             snippet += options;
+             snippet += "</ul>"
+             return snippet;
+        },
+	getAnnotatedText: function(response) {
+             var json = $.parseJSON(response);
+    
+             var text = json["annotation"]["@text"];
+
+             var start = 0;
+             var annotatedText = text; 
+            
+             if (json.annotation['surfaceForm']!=undefined)
+                  //console.log(json.annotation['surfaceForm']);
+                  var annotations = new Array().concat(json.annotation.surfaceForm) // deals with the case of only one surfaceFrom returned (ends up not being an array)
+                  annotatedText = annotations.map(function(e) {
+                  if (e==undefined) return "";
+		  //console.log(e);
+                  var sfName = e["@name"];
+                  var offset = parseInt(e["@offset"]);
+                  var sfLength = parseInt(sfName.length);
+                  var snippet = text.substring(start, offset)
+                  var surfaceForm = text.substring(offset,offset+sfLength);
+                  start = offset+sfLength;
+                  snippet += "<div id='"+(sfName+offset)+"' class='annotation'><a class='surfaceForm'>" + sfName + "</a>";
+                  //TODO instead of showing directly the select box, it would be cuter to just show a span, and onClick on that span, build the select box.
+                  snippet += Parser.getSelectBox($(e.resource),'candidate');
+                  snippet += "</div>";
+                  return snippet;
+			    }).join("");
+             //snippet after last surface form
+             annotatedText += text.substring(start, text.length);
+             //console.log(annotatedText);
+             return annotatedText;
+        },
+	getSuggestions: function(response, targetSurfaceForm) {
+             var json = $.parseJSON(response);
+             var suggestions = "";
+	     if (json.annotation['surfaceForm']!=undefined) {                  
+                  var annotations = new Array().concat(json.annotation.surfaceForm) // deals with the case of only one surfaceFrom returned (ends up not being an array)
+                  suggestions = annotations.filter(function(e) {
+			return (e["@name"]==$.trim(targetSurfaceForm)); 
+		  }).map(function(e) {
+                  	return Parser.getSelectBox($(e.resource),'none');
+		  }).join("");
+            }
+            return suggestions;
+        }
+   };
+
    var methods = {
       init : function( options ) {           
           // If options exist, lets merge them with our default settings
@@ -32,8 +92,9 @@
           }
       },
       best: function( options ) {
+          //init(options);
           function update(response) { 
-              console.log($(response));
+               //console.log($(response));
                var content = $(response).find("div");  //the div with the annotated text
                if (settings.powered_by == 'yes') { 
                    $(content).append($(powered_by)); 
@@ -43,9 +104,9 @@
           }    
        
                return this.each(function() {            
-                 console.log($.quoteString($(this).text()));
+                 //console.log($.quoteString($(this).text()));
                  var params = {'text': $(this).text(), 'confidence': settings.confidence, 'support': settings.support, 'spotter': settings.spotter };
-                 if("types" in settings){
+                 if("types" in settings && settings["types"] != undefined){
                    params["types"] = settings.types;
                  }
     
@@ -58,53 +119,9 @@
                  });
        },
        candidates: function( options ) {
-          function getSelectBox(resources) {
-             var snippet =  "<ul class='candidates'>";
-             console.log(resources);
-             var options = ""; $.each(resources, function(i, r) { 
-                 options += "<li class='candidate-" + i + "'><a href='" + r["@uri"] + "' about='" + r["@uri"] + "'>" + r["@label"] + "</a>";
-                 //TODO settings.showScores = ["finalScore"] foreach showscores, add k=v
-                 if (settings.showScores == 'yes') options += " <span>(" + parseFloat(r["@finalScore"]).toPrecision(3) +")</span>";
-                 options += "</li>"; 
-             });
-             snippet += options;
-             snippet += "</ul>"
-             return snippet;
-          }
-
-          
-          function parseCandidates(response) {
-             var json = $.parseJSON(response);
-    
-             var text = json["annotation"]["@text"];
-
-             var start = 0;
-             var annotatedText = text; 
-            
-             if (json.annotation['surfaceForm']!=undefined)  
-                  var annotations = new Array().concat(json.annotation.surfaceForm) // deals with the case of only one surfaceFrom returned (ends up not being an array)
-                  annotatedText = annotations.map(function(e) {
-                  //console.log(e);
-                  var sfName = e["@name"];
-                  var offset = parseInt(e["@offset"]);
-                  var sfLength = parseInt(sfName.length);
-                  var snippet = text.substring(start, offset)
-                  var surfaceForm = text.substring(offset,offset+sfLength);
-                  start = offset+sfLength;
-                  snippet += "<div id='"+(sfName+offset)+"' class='annotation'><a class='surfaceForm'>" + sfName + "</a>";
-                  //TODO instead of showing directly the select box, it would be cuter to just show a span, and onClick on that span, build the select box.
-                  snippet += getSelectBox($(e.resource));
-                  snippet += "</div>";
-                  return snippet;
-			    }).join("");
-             //snippet after last surface form
-             annotatedText += text.substring(start, text.length);
-             console.log(annotatedText);
-             return annotatedText;
-          }
-
+		//init(options);
                function update(response) { 
-                   var content = "<div style='overflow: auto'>" + parseCandidates(response) + "</div>";
+                   var content = "<div style='overflow: auto'>" + Parser.getAnnotatedText(response) + "</div>";
                    if (settings.powered_by == 'yes') { 
                        $(content).append($(powered_by)); 
                    };                        
@@ -124,13 +141,44 @@
                       'success': update
                     });
                });
+       },
+       suggest: function( options ) {
+	       //init(options);	
+               function update(response) {
+                   var keywords = Kolich.Selector.getSelected();  
+                   var suggestion = Parser.getSuggestions(response, keywords);
+			console.log('keywords:'+keywords);                   
+			console.log('suggestion:'+suggestion);
+                   var content = "<div style='overflow: auto'>" + suggestion + "</div>";
+                   if (settings.powered_by == 'yes') { 
+                       $(content).append($(powered_by)); 
+                   };                        
+                   $(this).html(content);          
+               }    
+       
+               return this.each(function() {            
+                 console.log('suggest');
+                 var keywords = $.trim("" + Kolich.Selector.getSelected());
+		 var params = {'text': $(this).text(), 'confidence': settings.confidence, 'support': settings.support, 'spotter': settings.spotter, 'surfaceForm': keywords }; 
+                 if("types" in settings && settings["types"] != undefined){
+                   params['types'] = settings.types;
+                 }
+    
+                 $.ajax({ 'url': settings.endpoint+"/candidates", 
+                      'data': params,
+                      'context': this,
+                      'headers': {'Accept': 'application/json'},
+                      'success': update
+                    });
+               });
        }
 
   }; 
   
   $.fn.annotate = function(method) {    
+	console.log('method:',method);
       // Method calling logic
-      if ( methods[method] ) {
+      if ( methods[method] ) {	
         return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
       } else if ( typeof method === 'object' || ! method ) {
         return methods.init.apply( this, arguments );
