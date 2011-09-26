08dfa5cfb9850146aa69bcfbe15035a9f05092c8
Joachim Daiber <jodaiber@gmx.de>
2011-09-26 15:48:53 +0000

    Added Spotter evaluation classes.

---
+++ eval/src/main/java/org/dbpedia/spotlight/evaluation/ExtractionEvaluator.java
@@ -0,0 +1,188 @@
+package org.dbpedia.spotlight.evaluation;
+
+import au.com.bytecode.opencsv.CSVReader;
+import org.apache.jcs.access.exception.CacheException;
+import org.dbpedia.spotlight.annotate.DefaultAnnotator;
+import org.dbpedia.spotlight.exceptions.ConfigurationException;
+import org.dbpedia.spotlight.exceptions.InputException;
+import org.dbpedia.spotlight.model.*;
+import org.dbpedia.spotlight.spot.cooccurrence.training.AnnotatedDataset;
+import org.json.JSONException;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author Joachim Daiber
+ */
+public class ExtractionEvaluator {
+
+	public static void main(String[] args) throws IOException, ConfigurationException, JSONException, InputException, CacheException {
+
+		SpotlightConfiguration configuration = new SpotlightConfiguration("conf/server.properties");
+		SpotlightFactory spotlightFactory = new SpotlightFactory(configuration);
+
+		AnnotatedDataset evaluationCorpus =
+				new AnnotatedDataset(new File("/Users/jodaiber/Desktop/csaw"),
+						AnnotatedDataset.Format.CSAW, spotlightFactory);
+
+		int tp = 0, fp = 0, fn = 0;
+
+		Set<DBpediaResourceOccurrence> goldAnnotations = evaluationCorpus.toDBpediaResourceOccurrences();
+		//writeAnnotations(spotlightFactory, evaluationCorpus,
+		//		new File("/Users/jodaiber/Documents/" +
+		//				"workspace/ba/BachelorThesis/02 Implementation/DBPedia Spotlight data/eval/candidate selection/csaw_ukwac/"));
+
+		/**
+		 * Read all annotations made by a configuration of DBpedia Spotlight.
+		 */
+		List<List<DBpediaResourceOccurrence>> allAnnotatedTexts = readAnnotations(new File("/Users/jodaiber/Documents/" +
+				"workspace/ba/BachelorThesis/02 Implementation/DBPedia Spotlight data/eval/candidate selection/csaw_ukwac/"),
+			evaluationCorpus);
+
+		/**
+		 * Remove all
+		 */
+		Set<DBpediaResourceOccurrence> allAnnotatedOccurrences = new HashSet<DBpediaResourceOccurrence>();
+		Set<Text> texts = new HashSet<Text>();
+		for(List<DBpediaResourceOccurrence> annotatedOccurrenceText : allAnnotatedTexts) {
+			if(annotatedOccurrenceText.size() == 0)
+				continue;
+
+			texts.add(annotatedOccurrenceText.get(0).context());
+			allAnnotatedOccurrences.addAll(annotatedOccurrenceText);
+
+			for(DBpediaResourceOccurrence dbpediaResourceOccurrence : annotatedOccurrenceText) {
+				if(goldAnnotations.contains(dbpediaResourceOccurrence))
+					tp++;
+				else
+					fp++;
+			}
+		}
+
+		/**
+		 * Remove all annotations whose texts have not been
+		 * annotated.
+		 */
+		Set<DBpediaResourceOccurrence> goldAnnotationsForTexts =  new HashSet<DBpediaResourceOccurrence>();
+		for(DBpediaResourceOccurrence goldAnnotation : goldAnnotations) {
+			if(texts.contains(goldAnnotation.context())) {
+				goldAnnotationsForTexts.add(goldAnnotation);
+			}
+		}
+
+
+		System.out.println("Gold annotations: " + goldAnnotations.size());
+		System.out.println("Gold annotations with annotated equivalent: " + goldAnnotationsForTexts.size());
+
+
+		for(DBpediaResourceOccurrence goldAnnotation : goldAnnotationsForTexts) {
+			if(!allAnnotatedOccurrences.contains(goldAnnotation))
+				fn++;
+		}
+
+
+		System.out.println("TP: " + tp);
+		System.out.println("FP: " + fp);
+		System.out.println("FN: " + fn);
+		
+		float precision = (tp / (float) (tp + fn));
+		System.out.println("P: " + precision);
+
+		float recall = (tp / (float) (tp + fp));
+		System.out.println("R: " + recall);
+		System.out.println("F1: " + ((2 * precision * recall)  / (recall+precision)));
+
+
+	}
+
+	private static List<List<DBpediaResourceOccurrence>> readAnnotations(File folder, AnnotatedDataset evaluationCorpus) {
+
+		File[] files = folder.listFiles();
+
+		List<List<DBpediaResourceOccurrence>> allAnnotatedOccurrences
+				= new LinkedList<List<DBpediaResourceOccurrence>>();
+
+		for(File file : files) {
+
+			if(!file.getName().endsWith(".tsv")) {
+				continue;
+			}
+
+			int textID = Integer.parseInt(file.getName().replace(".tsv", ""));
+
+			List<DBpediaResourceOccurrence> occurrencesInText = new ArrayList<DBpediaResourceOccurrence>();
+			try {
+				System.out.println("Read " + file);
+				CSVReader csvReader = new CSVReader(new FileReader(file), '\t');
+
+				System.out.println(evaluationCorpus.getTexts().size());
+				Text text = evaluationCorpus.getTexts().get(textID - 1);
+				System.out.println(text);
+
+				String[] line;
+				while((line = csvReader.readNext()) != null){
+
+					DBpediaResource dbpediaResource = new DBpediaResource(line[1]);
+					SurfaceForm surfaceForm = new SurfaceForm(line[2]);
+					int offset = Integer.parseInt(line[4]);
+
+					DBpediaResourceOccurrence dbpediaResourceOccurrence
+							= new DBpediaResourceOccurrence(dbpediaResource, surfaceForm, text, offset);
+					occurrencesInText.add(dbpediaResourceOccurrence);
+				}
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (IndexOutOfBoundsException e) {
+				/*
+				  A line in the CSV file could not be read, this is likely caused by the file not 
+				  being wrote properly, so we will ignore it.
+				 */
+				System.out.println("Error reading file " + file + e);
+				continue;
+			} catch (NumberFormatException e) {
+				/*
+				  A line in the CSV file could not be read, this is likely caused by the file not
+				  being wrote properly, so we will ignore it.
+				 */
+				System.out.println("Error reading file " + file + e);
+				continue;
+			}
+
+			System.out.println("Read file " + file);
+			allAnnotatedOccurrences.add(occurrencesInText);
+		}
+
+		return allAnnotatedOccurrences;
+	}
+
+	private static void writeAnnotations(SpotlightFactory spotlightFactory, AnnotatedDataset evaluationCorpus,
+										 File annotationsFolder) throws IOException {
+
+
+		DefaultAnnotator annotator = spotlightFactory.annotator();
+
+		int i = 0;
+		for(Text text : evaluationCorpus.getTexts()){
+			i++;
+			if(new File(annotationsFolder, i + ".tsv").exists()) {
+				continue;
+			}
+
+			try{
+				List<DBpediaResourceOccurrence> annotatedOccurrences = annotator.annotate(text.text());
+
+				FileWriter fileWriter = new FileWriter(new File(annotationsFolder, i + ".tsv"));
+				for(DBpediaResourceOccurrence annotatedOccurrence : annotatedOccurrences){
+					fileWriter.write(annotatedOccurrence.toTsvString() + "\n");
+				}
+				fileWriter.close();
+			}catch (InputException ignored) {}
+		}
+
+	}
+
+}
---
+++ eval/src/main/java/org/dbpedia/spotlight/evaluation/SpotterPerformanceEvaluator.java
@@ -0,0 +1,89 @@
+package org.dbpedia.spotlight.evaluation;
+
+import org.dbpedia.spotlight.spot.CoOccurrenceBasedSelector;
+import org.dbpedia.spotlight.exceptions.ConfigurationException;
+import org.dbpedia.spotlight.exceptions.InitializationException;
+import org.dbpedia.spotlight.model.SpotlightConfiguration;
+import org.dbpedia.spotlight.model.SpotlightFactory;
+import org.dbpedia.spotlight.model.SurfaceFormOccurrence;
+import org.dbpedia.spotlight.model.Text;
+import org.dbpedia.spotlight.spot.Spotter;
+import org.dbpedia.spotlight.spot.SpotterWithSelector;
+import org.dbpedia.spotlight.spot.lingpipe.LingPipeSpotter;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Joachim Daiber
+ */
+public class SpotterPerformanceEvaluator {
+
+
+	public static void main(String[] args) throws InitializationException, ConfigurationException, IOException, JSONException {
+
+		SpotlightConfiguration configuration = new SpotlightConfiguration("conf/server.properties");
+		SpotlightFactory spotlightFactory = new SpotlightFactory(configuration);
+
+
+		/**
+		 * Read test corpus:
+		 */
+
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(
+				new File("/Users/jodaiber/Documents/workspace/ba/BachelorThesis/LaTeX/figures/performance/corpus.json")
+		));
+		String jsonString = bufferedReader.readLine();
+		JSONObject jsonObject = new JSONObject(jsonString);
+
+
+		///**
+		// * No selection:
+		// */
+		Spotter spotter = new LingPipeSpotter(new File(configuration.getSpotterConfiguration().getSpotterFile()));
+		//
+		//extract(jsonObject, spotter);
+
+
+		/**
+		 * Advanced Spotter:
+		 */
+		Spotter spotterWithSelector = SpotterWithSelector.getInstance(
+				spotter,
+				new CoOccurrenceBasedSelector(configuration.getSpotterConfiguration()),
+				spotlightFactory.taggedTokenProvider()
+		);
+
+		extract(jsonObject, spotterWithSelector);
+
+	}
+
+	private static void extract(JSONObject jsonObject, Spotter spotter) throws JSONException {
+		Iterator keys = jsonObject.keys();
+		while(keys.hasNext()) {
+			String domain = (String) keys.next();
+			JSONArray texts = jsonObject.getJSONArray(domain);
+			List<SurfaceFormOccurrence> extracted = new LinkedList<SurfaceFormOccurrence>();
+
+			long start = System.currentTimeMillis();
+			for(int i = 0; i < texts.length(); i++) {
+				extracted.addAll(spotter.extract(new Text(texts.getString(i))));
+			}
+			long end = System.currentTimeMillis();
+
+			System.out.println("Domain: " + domain);
+			System.out.println("# occurrences: " + extracted.size());
+			System.out.println("Time: " + (end - start) + " ms");
+
+		}
+	}
+
+}
