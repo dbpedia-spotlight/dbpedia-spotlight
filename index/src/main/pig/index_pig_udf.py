b1353a1deb70b3d3296b4155fa0a7dd700b98ba3
hunterhector <hunterhector@gmail.com>
2012-05-23 03:37:52 +0800

    Update Pig Script to output in adjacency list like format (in PigStorage, not JsonStorage)

--- index/src/main/pig/index_pig_udf.py
+++ index/src/main/pig/index_pig_udf.py
@@ -42,13 +42,10 @@ def getDocParaId(fullId):
 ####################
 # Get Combinations #
 ####################
-@outputSchema("y:bag{t:tuple(ent1:chararray,ent2:chararray)}")
+@outputSchema("combination:bag{entityPair:tuple(ent1:chararray,ent2:chararray)}")
 def getCombination(bag):
 	outBag = []
 	for i in range(len(bag)):
 		for j in range(i+1,len(bag)):
-			if bag[i] < bag[j]:
 				outBag.append((bag[i][0],bag[j][0]))
-			else:
-				outBag.append((bag[j][0],bag[i][0]))
 	return outBag
