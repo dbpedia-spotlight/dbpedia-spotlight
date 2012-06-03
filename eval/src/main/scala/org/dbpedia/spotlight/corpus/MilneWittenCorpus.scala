3ab86b6b9efc0e423d1a73c1f53472eaeed1eb6a
Pablo Mendes <pablo.mendes@fu-berlin.de>
2012-06-03 20:13:00 +0200

    cleanup m&w corpus reader code

--- eval/src/main/scala/org/dbpedia/spotlight/corpus/MilneWittenCorpus.scala
+++ eval/src/main/scala/org/dbpedia/spotlight/corpus/MilneWittenCorpus.scala
@@ -70,7 +70,7 @@ class MilneWittenCorpus(val documents: Traversable[NodeSeq]) extends AnnotatedTe
             val confidence = if (wikiLinkArray.length>2) wikiLinkArray(2).toDouble else 1.0
             val lengthDifference = wikiLinkString.length - cleanSurfaceForm.length
             val offset = m.start - accumulatedLengthDifference // this is where the clean surface form starts in the clean text
-            //                println(cleanSurfaceForm+"="+cleanText.toString.substring(offset,offset+cleanSurfaceForm.length))
+
             if (offset equals 0) {
                 cleanText.append(m.before)
             } else {
@@ -80,8 +80,6 @@ class MilneWittenCorpus(val documents: Traversable[NodeSeq]) extends AnnotatedTe
 
             lastDirtyOffset = m.end
 
-            //last chunk
-            //if (!m.after.toString.contains("""\[\[|\]\]"""))
             if (i==nMatches)
 		cleanText.append(m.after.toString)
 
@@ -109,42 +107,4 @@ object MilneWittenCorpus {
         })
     }
 
-    def test(dir: File) = {
-        val files = dir.listFiles.filter(f => """.*\.htm$""".r.findFirstIn(f.getName).isDefined)
-        //files.foreach(println)
-        val root = scala.xml.parsing.XhtmlParser(Source.fromFile("/home/pablo/eval/wikify/original/APW19980625_1136.htm"))
-        val paragraphs = root.map( _ \ "body" \ "p" ).map( _.text.trim )
-
-        val wikiLinkMatcher = """\[\[(.*?)\]\]""".r
-
-        paragraphs.foreach( p => {
-            val cleanText = new StringBuffer()
-            var accumulatedLengthDifference = 0
-            var lastDirtyOffset = 0
-            (wikiLinkMatcher findAllIn p).matchData foreach { m => {  // println(m.start, m.end, m.before, m.after, m.source)
-
-                val wikiLinkString = m.toString //Example: [[The Guardian (Nigeria)|Guardian newspaper|0.4]])
-                val wikiLinkArray = wikiLinkString.replaceAll("""\[\[|\]\]""","").split("\\|")
-                val uri = wikiLinkArray(0)
-                val cleanSurfaceForm = if (wikiLinkArray.length>1) wikiLinkArray(1) else wikiLinkArray(0)
-                val confidence = if (wikiLinkArray.length>2) wikiLinkArray(2).toDouble else 1.0
-                val lengthDifference = wikiLinkString.length - cleanSurfaceForm.length
-                val offset = m.start - accumulatedLengthDifference // this is where the clean surface form starts in the clean text
-//                println(cleanSurfaceForm+"="+cleanText.toString.substring(offset,offset+cleanSurfaceForm.length))
-                if (offset equals 0) {
-                    cleanText.append(m.before)
-                } else {
-                    cleanText.append(m.before.toString.substring(lastDirtyOffset,m.start))
-                }
-                cleanText.append(cleanSurfaceForm)
-
-                lastDirtyOffset = m.end
-                accumulatedLengthDifference = accumulatedLengthDifference + lengthDifference
-
-                (uri,cleanSurfaceForm,offset,confidence)
-            }}
-            //println(cleanText)
-        });
-
-    }
 }
