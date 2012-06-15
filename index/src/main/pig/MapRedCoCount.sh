45e1aadfb1737db615a274e167ab41bd95301475
hunterhector <hunterhector@gmail.com>
2012-06-16 03:03:38 +0800

    -getmerge can get one complete file

--- index/src/main/pig/MapRedCoCount.sh
+++ index/src/main/pig/MapRedCoCount.sh
@@ -1,3 +1,3 @@
 hadoop fs -copyFromLocal ../../../output/occs.tsv .
 pig -param inFile=occs.tsv -param outDir=. CooccurrencesCount.pig
-hadoop fs -get co-occs-count.tsv ../../../output/co-occs-count.tsv
+hadoop fs -getmerge co-occs-count.tsv ../../../output/co-occs-count.tsv
