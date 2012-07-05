1bd7201d5b884723937eb11ee89532a85c220adc
Jo Daiber <jodaiber@gmx.de>
2012-07-05 13:06:29 +0200

    HashCode and equals for Token.

--- core/src/main/scala/org/dbpedia/spotlight/model/Token.scala
+++ core/src/main/scala/org/dbpedia/spotlight/model/Token.scala
@@ -9,6 +9,15 @@ package org.dbpedia.spotlight.model
 
 class Token(val id: Int, val name: String, val count: Int) {
 
+  override def hashCode() = name.hashCode
+
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case token: Token => name.equals(token.name)
+      case _ => false
+    }
+  }
+
   override def toString = "%s (%d)".format(name, count)
 
 }
