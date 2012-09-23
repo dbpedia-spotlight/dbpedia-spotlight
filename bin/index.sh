fdd9fb0ba483da83a3b56dc0466c65318482f7e8
Pablo Mendes <pablo.mendes@fu-berlin.de>
2012-09-23 15:40:49 +0200

    fixes to scripts using tab as separators
    * merged getSurfaceFormMapFromOccs.sh into index.sh
    * used tabs as $'\t'
    * added note about out of memory error that happens when heap size is not set correctly. so far, it seems that it is only possible to set within the pom.xml

--- bin/index.sh
+++ bin/index.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # You are expected to run the commands in this script from inside the bin directory in your DBpedia Spotlight installation
 # Adjust the paths here if you don't. This script is meant more as a step-by-step guidance than a real automated run-all.
 # If this is your first time running the script, we advise you to copy/paste commands from here, closely watching the messages
@@ -5,7 +6,7 @@
 #
 # @author maxjakob, pablomendes
 
-export DBPEDIA_WORKSPACE=/usr/local/spotlight/dbpedia_data
+export DBPEDIA_WORKSPACE=/var/local/spotlight/dbpedia_data
 
 
 export INDEX_CONFIG_FILE=../conf/indexing.properties
@@ -14,10 +15,9 @@ export INDEX_CONFIG_FILE=../conf/indexing.properties
 # NOTE: If setting the variables below is not correctly increasing your heap size, try setting it in the <jvmArg> in index/pom.xml
 # See: https://groups.google.com/forum/?fromgroups=#!topic/scalatest-users/P6lXSxI4Nmg
 ulimit -v unlimited
-export JAVA_OPTS="-Xmx14G  -XX:+UseConcMarkSweepGC -XX:MaxPermSize=7G"
-export MAVEN_OPTS="-Xmx14G -XX:+UseConcMarkSweepGC -XX:MaxPermSize=7G"
-export SCALA_OPTS="-Xmx14G -XX:+UseConcMarkSweepGC -XX:MaxPermSize=7G"
-
+set JAVA_OPTS="-Xmx10G  -XX:+UseConcMarkSweepGC -XX:MaxPermSize=7G"; export JAVA_OPTS;
+set MAVEN_OPTS="-Xmx10G -XX:+UseConcMarkSweepGC -XX:MaxPermSize=7G"; export MAVEN_OPTS;
+set SCALA_OPTS="-Xmx10G -XX:+UseConcMarkSweepGC -XX:MaxPermSize=7G"; export SCALA_OPTS;
 ulimit -v unlimited
 
 # you have to run maven2 from the module that contains the indexing classes
@@ -33,29 +33,37 @@ fi
 mvn scala:run -DmainClass=org.dbpedia.spotlight.util.ExtractCandidateMap "-DaddArgs=$INDEX_CONFIG_FILE"
 
 # now we collect parts of Wikipedia dump where DBpedia resources occur and output those occurrences as Tab-Separated-Values
+echo -e "Parsing Wikipedia dump to extract occurrences...\n"
 mvn scala:run -DmainClass=org.dbpedia.spotlight.lucene.index.ExtractOccsFromWikipedia "-DaddArgs=$INDEX_CONFIG_FILE|$DBPEDIA_WORKSPACE/data/output/occs.tsv"
 
 # (recommended) sorting the occurrences by URI will speed up context merging during indexing
-#bug for join with -t\t ...
-tab=$(printf \t)
-sort -t "$tab" -k2 $DBPEDIA_WORKSPACE/data/output/occs.tsv >$DBPEDIA_WORKSPACE/data/output/occs.uriSorted.tsv
-
-# create a lucene index out of the occurrences
-mvn scala:run -DmainClass=org.dbpedia.spotlight.lucene.index.IndexMergedOccurrences "-DaddArgs=$INDEX_CONFIG_FILE|$DBPEDIA_WORKSPACE/data/output/occs.uriSorted.tsv"
-
-# (optional) make a backup copy of the index before you lose all the time you've put into this
-cp -R $DBPEDIA_WORKSPACE/data/output/index $DBPEDIA_WORKSPACE/data/output/index-backup
+echo -e "Sorting occurrences to speed up indexing...\n"
+sort -t$'\t' -k2 $DBPEDIA_WORKSPACE/data/output/occs.tsv >$DBPEDIA_WORKSPACE/data/output/occs.uriSorted.tsv
 
 # (optional) preprocess surface forms however you want: produce acronyms, abbreviations, alternative spellings, etc.
 #            in the example below we scan paragraphs for uri->sf mappings that occurred together more than 3 times.
-../bin/getSurfaceFormMapFromOccs.sh
+echo -e "Extracting Surface Forms...\n"
+cat $DBPEDIA_WORKSPACE/data/output/occs.uriSorted.tsv | cut -d$'\t' -f 2,3 |  perl -F/\\t/ -lane 'print "$F[1]\t$F[0]";' > $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromOccs.tsv
+sort $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromOccs.tsv | uniq -c > $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromOccs.count
+grep -Pv "      [123] " $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromOccs.count | sed -r "s|\s+[0-9]+\s(.+)|\1|" > $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromOccs-thresh3.tsv
+
 cp $DBPEDIA_WORKSPACE/data/output/surfaceForms.tsv $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromTitRedDis.tsv
-cat $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromTitRedDis.tsv $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromOccs.tsv > $DBPEDIA_WORKSPACE/data/output/surfaceForms.tsv
+cat $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromTitRedDis.tsv $DBPEDIA_WORKSPACE/data/output/surfaceForms-fromOccs-thresh3.tsv | sort -u > $DBPEDIA_WORKSPACE/data/output/surfaceForms.tsv
 
 # now that we have our set of surfaceForms, we can build a simple dictionary-based spotter from them
-mvn scala:run -DmainClass= org.dbpedia.spotlight.spot.lingpipe.IndexLingPipeSpotter "-DaddArgs=$INDEX_CONFIG_FILE"
+mvn scala:run -DmainClass=org.dbpedia.spotlight.spot.lingpipe.IndexLingPipeSpotter "-DaddArgs=$INDEX_CONFIG_FILE"
 
+set -e
+# create a lucene index out of the occurrences
+echo -e "Creating a context index from occs.tsv...\n"
+mvn scala:run -DmainClass=org.dbpedia.spotlight.lucene.index.IndexMergedOccurrences "-DaddArgs=$INDEX_CONFIG_FILE|$DBPEDIA_WORKSPACE/data/output/occs.uriSorted.tsv"
+# NOTE: if you get an out of memory error from the command above, try editing ../index/pom.xml with correct jvmArg and file arguments, then run:
+#mvn scala:run -Dtarget=indexer "-DaddArgs=$INDEX_CONFIG_FILE|$DBPEDIA_WORKSPACE/data/output/occs.uriSorted.tsv"
+
+# (optional) make a backup copy of the index before you lose all the time you've put into this
+#cp -R $DBPEDIA_WORKSPACE/data/output/index $DBPEDIA_WORKSPACE/data/output/index-backup
 # add surface forms to index
+echo -e "Adding Surface Forms to index...\n"
  mvn scala:run -DmainClass=org.dbpedia.spotlight.lucene.index.AddSurfaceFormsToIndex "-DaddArgs=$INDEX_CONFIG_FILE|$DBPEDIA_WORKSPACE/data/output/index"
 # or
  mvn scala:run -DmainClass=org.dbpedia.spotlight.lucene.index.CandidateIndexer "-DaddArgs=$DBPEDIA_WORKSPACE/data/output/surfaceForms.tsv|$DBPEDIA_WORKSPACE/data/output/candidateIndex|3|case-insensitive|overwrite"
@@ -65,6 +73,7 @@ mvn scala:run -DmainClass=org.dbpedia.spotlight.lucene.index.AddTypesToIndex "-D
 
 # (optional) reduce index size by unstoring fields (attention: you won't be able to see contents of fields anymore)
 mvn scala:run -DmainClass=org.dbpedia.spotlight.lucene.index.CompressIndex "-DaddArgs=$INDEX_CONFIG_FILE|10|$DBPEDIA_WORKSPACE/data/output/index-withSF-withTypes"
+set +e
 
 # train a linker (most simple is based on similarity-thresholds)
 # mvn scala:run -DmainClass=org.dbpedia.spotlight.evaluation.EvaluateDisambiguationOnly
