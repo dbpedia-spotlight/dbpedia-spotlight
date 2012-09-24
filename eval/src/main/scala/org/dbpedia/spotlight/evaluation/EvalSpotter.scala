f505f3f1ebbed8001b071ff6daba3fdb2ae8c50e
Max Jakob <max.jakob@gmail.com>
2012-09-25 01:09:11 +0200

    small re-factoring in EvalSpotter

--- eval/src/main/scala/org/dbpedia/spotlight/evaluation/EvalSpotter.scala
+++ eval/src/main/scala/org/dbpedia/spotlight/evaluation/EvalSpotter.scala
@@ -28,18 +28,22 @@ object EvalSpotter {
     }
 
     def evalSpotting(annotatedTextSource: AnnotatedTextSource) {
-        evalSpotting(annotatedTextSource, new SimpleAnalyzer(Version.LUCENE_36))
-        evalSpotting(annotatedTextSource, new StopAnalyzer(Version.LUCENE_36))
-        evalSpotting(annotatedTextSource, new ClassicAnalyzer(Version.LUCENE_36))
-        evalSpotting(annotatedTextSource, new StandardAnalyzer(Version.LUCENE_36))
-        evalSpotting(annotatedTextSource, new EnglishAnalyzer(Version.LUCENE_36))
-        evalSpotting(annotatedTextSource, new WhitespaceAnalyzer(Version.LUCENE_36))
+        val analyzers = List(
+            new SimpleAnalyzer(Version.LUCENE_36),
+            new StopAnalyzer(Version.LUCENE_36),
+            new ClassicAnalyzer(Version.LUCENE_36),
+            new StandardAnalyzer(Version.LUCENE_36),
+            new EnglishAnalyzer(Version.LUCENE_36),
+            new WhitespaceAnalyzer(Version.LUCENE_36)
+        )
+        for (analyzer <- analyzers) {
+            evalSpotting(annotatedTextSource, analyzer)
+        }
     }
 
     def evalSpotting(annotatedTextSource: AnnotatedTextSource, analyzer: Analyzer) {
+        // create gold standard and index
         var expected = Set[SurfaceFormOccurrence]()
-
-        // index
         val dictionary = new MapDictionary[String]()
         for (paragraph <- annotatedTextSource;
              occ <- paragraph.occurrences) {
@@ -54,25 +58,27 @@ object EvalSpotter {
             actual = JavaConversions.asScalaBuffer(lingPipeSpotter.extract(paragraph.text)).toSet union actual
         }
 
+        printResults("LingPipeSpotter with %s and corpus %s".format(analyzer.getClass, annotatedTextSource.name),
+            expected, actual)
+    }
+
+    def printResults(description: String, expected: Set[SurfaceFormOccurrence], actual: Set[SurfaceFormOccurrence]) {
         var truePositive = 0
         var falseNegative = 0
         for (e <- expected) {
             if (actual contains e) {
                 truePositive += 1
-                actual = actual - e
             } else {
                 falseNegative += 1
                 LOG.debug("false negative: " + e)
             }
         }
-        val falsePositive = actual.size
+        val falsePositive = actual.size - truePositive
 
         val precision = truePositive.toDouble / (truePositive + falseNegative)
         val recall = truePositive.toDouble / (truePositive + falsePositive)
 
-        LOG.info("Corpus: " + annotatedTextSource.name)
-        LOG.info("Spotter: " + lingPipeSpotter.getName)
-        LOG.info("Analyzer: " + analyzer.getClass)
+        LOG.info(description)
         LOG.info("           | actual Y  | actual N")
         LOG.info("expected Y |   %3d     |    %3d".format(truePositive, falseNegative))
         LOG.info("expected N |   %3d     |    N/A".format(falsePositive))
