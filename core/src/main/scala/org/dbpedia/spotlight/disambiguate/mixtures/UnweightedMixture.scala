b3655b8f1ed45cfe6b02004e6eca534c5197c84b
jodaiber <jodaiber@gmx.de>
2013-01-07 12:45:58 +0100

    Fixes build error in UnweightedMixture.

--- core/src/main/scala/org/dbpedia/spotlight/disambiguate/mixtures/UnweightedMixture.scala
+++ core/src/main/scala/org/dbpedia/spotlight/disambiguate/mixtures/UnweightedMixture.scala
@@ -1,7 +1,7 @@
 package org.dbpedia.spotlight.disambiguate.mixtures
 
 import org.dbpedia.spotlight.model.{Feature, DBpediaResourceOccurrence}
-import org.dbpedia.spotlight.util.Logs
+import org.dbpedia.spotlight.util.MathUtil
 
 /**
  * Multiplication of scores/probabilities. Assumes probabilities are logarithms.
@@ -13,7 +13,7 @@ class UnweightedMixture(features: Set[String]) extends Mixture(1) {
 
   def getScore(occurrence: DBpediaResourceOccurrence): Double = {
     val fs = occurrence.features.values.filter({ f: Feature => features.contains(f.featureName) }).map(_.value.asInstanceOf[Double])
-    Logs.lnproduct(fs)
+    MathUtil.lnproduct(fs)
   }
 
   override def toString = "UnweightedMixture[%s]".format(features.mkString(","))
