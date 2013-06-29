114df62ecd98f9043af49ccd4805e3ecb8e98c14
wangwei <wangwei.cs@gmail.com>
2013-06-29 15:58:48 +0800

    add the wiki parser code from pignlp; pre-process wiki data; draft document init assignment code

--- core/src/main/scala/org/dbpedia/spotlight/db/entityTopic/DocumentStore.scala
+++ core/src/main/scala/org/dbpedia/spotlight/db/entityTopic/DocumentStore.scala
@@ -1,12 +1,42 @@
 package org.dbpedia.spotlight.db.entityTopic
 
-/**
- * Created with IntelliJ IDEA.
- * User: a0082293
- * Date: 6/28/13
- * Time: 10:12 PM
- * To change this template use File | Settings | File Templates.
- */
-class DocumentStore {
+import com.esotericsoftware.kryo.io.Output
+import java.io.{FileInputStream, FileOutputStream}
+import org.dbpedia.spotlight.db.memory.MemoryStore
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.io.{Output, Input}
 
+
+class DocumentStore {
+  var output:Output =null
+  var input: Input=null
+  var kryo: Kryo=null
+
+  val filename:String="documentStore.dat"
+
+  def initSave(){
+    output= new Output(new FileOutputStream(filename))
+    kryo=MemoryStore.kryos.get(classOf[Document].getSimpleName).get
+  }
+  def save(doc:Document){
+    kryo.writeClassAndObject(output,doc)
+  }
+
+  def finishSave(){
+    output.close()
+  }
+
+  def initLoad(){
+    input= new Input(new FileInputStream(filename))
+    kryo=MemoryStore.kryos.get(classOf[Document].getSimpleName).get
+  }
+
+  def load():Document={
+    val doc=kryo.readClassAndObject(input)
+    doc.asInstanceOf[Document]
+  }
+
+  def finishLoad(){
+    input.close()
+  }
 }
