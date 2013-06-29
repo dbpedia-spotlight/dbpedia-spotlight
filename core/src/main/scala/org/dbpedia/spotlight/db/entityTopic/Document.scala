bbaa86290be94ab4c4fefe483f793167e73da22f
wangwei <wangwei.cs@gmail.com>
2013-06-29 15:58:07 +0800

    pom clean-up and plugin version updates
    - only use properties for dependencies with multiple artifacts
    -- lucene
    -- jersey
    
    - updated plugin versions:
    -- scala-maven-plugin: 3.1.5
    -- maven-compiler-plugin: 3.1
    -- maven-surefire-plugin: 2.14.1
    -- maven-enforcer-plugin: 1.2
    -- maven-install-plugin: 2.4
    -- maven-assembly-plugin: 2.4
    -- org.vafer: 0.8

---
+++ core/src/main/scala/org/dbpedia/spotlight/db/entityTopic/Document.scala
@@ -0,0 +1,12 @@
+package org.dbpedia.spotlight.db.entityTopic
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: a0082293
+ * Date: 6/28/13
+ * Time: 10:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Document {
+
+}
