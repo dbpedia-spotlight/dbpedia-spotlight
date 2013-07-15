c911b1e54ba079cad58c8badb42a557a19efe8d2
wangwei <wangwei.cs@gmail.com>
2013-07-15 11:14:03 +0800

    implement the global counter by sparse matrix

--- core/src/main/scala/org/dbpedia/spotlight/db/entityTopic/GlobalCounter.scala
+++ core/src/main/scala/org/dbpedia/spotlight/db/entityTopic/GlobalCounter.scala
@@ -1,29 +1,39 @@
 package org.dbpedia.spotlight.db.entitytopic
 
 import java.util.HashMap
+import breeze.linalg.CSCMatrix
 
 /**
- * Created with IntelliJ IDEA.
- * User: a0082293
- * Date: 7/7/13
- * Time: 3:32 PM
- * To change this template use File | Settings | File Templates.
+ * Global Counter implemented by sparse matrix from breeze
+ * count is the matrix
+ * countSum is the sum of entries in one row
+ *
+ * @param rows
+ * @param cols
  */
-class GlobalCounter {
-  val count: HashMap[(Int,Int),Int]=new HashMap[(Int,Int),Int]()
+
+class GlobalCounter(rows:Int, cols:Int) {
+  val count: CSCMatrix[Int]=CSCMatrix.zeros(rows,cols)
   val countSum: HashMap[Int,Int]=new HashMap[Int, Int]()
 
   def incCount(first: Int, second: Int){
-
+    count.update(first,second,count(first,second)+1)
+    countSum.get(first) match{
+      case num:Int=>countSum.put(first,num+1)
+      case _=>countSum.put(first,0)
+    }
   }
 
   def decCount(first: Int, second: Int){
-
+    count.update(first,second,count(first,second)-1)
+    countSum.get(first) match{
+      case num:Int=>countSum.put(first,num-1)
+      case _=>countSum.put(first,0)
+    }
   }
 
   def getCount(first: Int, second: Int):Int={
-
-    count.get((first,second))
+    count(first,second)
   }
 
   def getCountSum(first: Int):Int={
