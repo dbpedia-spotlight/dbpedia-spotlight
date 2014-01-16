8dd5cf112adb0e06bd4040ead4a98e75778c9b0f
accardoso <acc.intrisic@gmail.com>
2014-01-16 19:52:09 -0200

    Unit test for class Factory

--- core/src/test/scala/org/dbpedia/spotlight/model/FactoryTest.scala
+++ core/src/test/scala/org/dbpedia/spotlight/model/FactoryTest.scala
@@ -4,18 +4,9 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
-import org.dbpedia.spotlight.lucene.search.BaseSearcher
-import org.dbpedia.spotlight.lucene.LuceneManager
-
-import java.io.File
-import org.apache.lucene.index.IndexReader
-import org.apache.lucene.document.Document
-import org.dbpedia.spotlight.model.Factory
-
-//import org.dbpedia.spotlight.util.IndexingConfiguration
-import org.apache.lucene.store.FSDirectory
 
 /**
+ * The unit tests for Factory class.
  *
  * @author Alexandre Cançado Cardoso - accardoso
  */
@@ -24,61 +15,60 @@ import org.apache.lucene.store.FSDirectory
 class FactoryTest extends FlatSpec with ShouldMatchers {
 
   /* Test Factory.SurfaceForm */
-
   "The correct Surface Form" should "be created for the surface form string 'Berlin'" in {
     val surfaceFormString = "Berlin"
-    Factory.SurfaceForm.fromString(surfaceFormString).equals(new SurfaceForm(surfaceFormString)) should be === true
+    Factory.SurfaceForm.fromString(surfaceFormString).name should be === (new SurfaceForm(surfaceFormString)).name
   }
 
   it should "be created for the uri 'http://dbpedia.org/resource/Berlin'" in {
     val uriString = "http://dbpedia.org/resource/Berlin"
-    Factory.SurfaceForm.fromDBpediaResourceURI(uriString, false).equals(new SurfaceForm(uriString)) should be === true
+    Factory.SurfaceForm.fromDBpediaResourceURI(uriString, false).name should be === (new SurfaceForm(uriString)).name
   }
 
   it should "be created for the uri 'Berlin'" in {
     val uriString = "Berlin"
-    Factory.SurfaceForm.fromDBpediaResourceURI(uriString, false).equals(new SurfaceForm(uriString)) should be === true
+    Factory.SurfaceForm.fromDBpediaResourceURI(uriString, true).name should be === (new SurfaceForm(uriString)).name.toLowerCase
   }
 
   it should "be created for 'DBpediaResource[Berlin]'" in {
     val uriString = "Berlin"
     val uri = new DBpediaResource("Berlin")
-    Factory.SurfaceForm.fromDBpediaResourceURI(uri, false).equals(new SurfaceForm(uriString)) should be === true
+    Factory.SurfaceForm.fromDBpediaResourceURI(uri, false).name should be === (new SurfaceForm(uriString)).name
   }
 
   it should "be created for the full uri of 'DBpediaResource[Berlin]'" in {
     val uriString = "Berlin"
     val uri = new DBpediaResource("Berlin")
-    Factory.SurfaceForm.fromDBpediaResourceURI(uri.uri, false).equals(new SurfaceForm(uriString)) should be === true
+    Factory.SurfaceForm.fromDBpediaResourceURI(uri.uri, false).name should be === (new SurfaceForm(uriString)).name
   }
 
   it should "be created for the url 'http://en.wikipedia.org/resource/Berlin'" in {
     val surfaceFormString = "Berlin"
     val url = "http://en.wikipedia.org/resource/Berlin"
-    Factory.SurfaceForm.fromWikiPageTitle(url, false).equals(new SurfaceForm(surfaceFormString)) should be === true
+    Factory.SurfaceForm.fromWikiPageTitle(FactoryTest.extractPageTitleFrom(url), false).name should be === (new SurfaceForm(surfaceFormString)).name
   }
 
-  it should "be created for the url 'http://pt.wikipedia.org/resource/Berlim'" in {
-    val surfaceFormString = "Berlim"
-    val url = "http://pt.wikipedia.org/resource/Berlim"
-    Factory.SurfaceForm.fromWikiPageTitle(url, false).equals(new SurfaceForm(surfaceFormString)) should be === true
+  /* Test Factory.SurfaceFormOccurrence */
+
+  it should "todo" in {
+    val uriString = "http://dbpedia.org/resource/Berlin"
+    val sf = new SurfaceForm("Berlin")
+    val context = new Text("Berlin is a capital.")
+    val offset = 1
+    val inputDBpediaResourceOcc = new DBpediaResourceOccurrence(new DBpediaResource(uriString), sf, context, offset)
+    val expectedSFOcc = new SurfaceFormOccurrence(sf, context, offset)
+    Factory.SurfaceFormOccurrence.from(inputDBpediaResourceOcc).equals(expectedSFOcc) should be === true
   }
-
-  it should "be created for the url 'http://en.wikipedia.org/resource/Berlim'" in {
-    val surfaceFormString = "Berlim"
-    val url = "http://en.wikipedia.org/resource/Berlim"
-    Factory.SurfaceForm.fromWikiPageTitle(url, false).equals(new SurfaceForm(surfaceFormString)) should be === true
-  }
-
-//  it should "be created for the url 'http://ontologia.globo.com/resource/Berlim'" in {
-//    val surfaceFormString = "Berlim"
-//    val url = "http://ontologia.globo.com/resource/Berlim"
-//    Factory.SurfaceForm.fromWikiPageTitle(url, false).equals(new SurfaceForm(surfaceFormString)) should be === true
-//  }
-
 
   /* Test Factory.DBpediaResourceOccurrence */
+  //This already existing test are implemented at before existing class FactoryTests
 
+}
 
+object FactoryTest {
+
+  def extractPageTitleFrom(url: String): String = {
+    url.substring(url.lastIndexOf('/')+1)
+  }
 
 }
