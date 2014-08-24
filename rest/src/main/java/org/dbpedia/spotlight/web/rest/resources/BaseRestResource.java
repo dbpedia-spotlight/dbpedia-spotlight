39f90e0b06426deffd7111508d559d2f1bcedb7a
David Przybilla <dav.alejandro@gmail.com>
2014-08-24 01:29:38 +0100

    commenting announce

--- rest/src/main/java/org/dbpedia/spotlight/web/rest/resources/BaseRestResource.java
+++ rest/src/main/java/org/dbpedia/spotlight/web/rest/resources/BaseRestResource.java
@@ -13,20 +13,20 @@ public class BaseRestResource {
     String apiName = "BaseRestResource";
 
     public void announce(String textString, AnnotationParameters params) {
-        LOG.info("******************************** Parameters ********************************");
-        LOG.info("API: " + getApiName());
-        LOG.info("client ip: " + params.clientIp);
-        LOG.info("text: " + textString);
-        LOG.info("text length in chars: " + textString.length());
-        LOG.info("disambiguation confidence: "+ params.disambiguationConfidence);
-        LOG.info("spotterConfidence confidence: "+ params.spotterConfidence);
-        LOG.info("support: "+ String.valueOf(params.support));
-        LOG.info("types: "+ params.dbpediaTypes);
-        LOG.info("sparqlQuery: "+ params.sparqlQuery);
-        LOG.info("policy: "+ params.policy);
-        LOG.info("coreferenceResolution: "+params.coreferenceResolution);
-        LOG.info("spotter: "+ params.spotterName);
-        LOG.info("disambiguator: "+ params.disambiguatorName);
+//        LOG.info("******************************** Parameters ********************************");
+//        LOG.info("API: " + getApiName());
+//        LOG.info("client ip: " + params.clientIp);
+//        LOG.info("text: " + textString);
+//        LOG.info("text length in chars: " + textString.length());
+//        LOG.info("disambiguation confidence: "+ params.disambiguationConfidence);
+//        LOG.info("spotterConfidence confidence: "+ params.spotterConfidence);
+//        LOG.info("support: "+ String.valueOf(params.support));
+//        LOG.info("types: "+ params.dbpediaTypes);
+//        LOG.info("sparqlQuery: "+ params.sparqlQuery);
+//        LOG.info("policy: "+ params.policy);
+//        LOG.info("coreferenceResolution: "+params.coreferenceResolution);
+//        LOG.info("spotter: "+ params.spotterName);
+//        LOG.info("disambiguator: "+ params.disambiguatorName);
 
     }
 
