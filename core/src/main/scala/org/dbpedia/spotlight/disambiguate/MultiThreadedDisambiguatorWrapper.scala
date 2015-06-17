54b8e7723b84d73ebc82acc4f8b30df458663628
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-06-17 11:34:48 +0200

    trying to fix breeze errors

--- core/src/main/scala/org/dbpedia/spotlight/disambiguate/MultiThreadedDisambiguatorWrapper.scala
+++ core/src/main/scala/org/dbpedia/spotlight/disambiguate/MultiThreadedDisambiguatorWrapper.scala
@@ -34,13 +34,14 @@ package org.dbpedia.spotlight.disambiguate
  * limitations under the License.
  */
 
+
+import akka.actor._
+
+
 import scala.collection.JavaConverters._
 import org.dbpedia.spotlight.log.SpotlightLog
 import org.apache.lucene.search.Explanation
 import org.dbpedia.spotlight.model._
-
-import scala.actors._
-import Actor._
 
 import org.dbpedia.spotlight.exceptions.{DisambiguationException, SearchException, InputException}
 
