eb28c8af56e65a6472e6439a0d5439113502ec74
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-07-10 01:14:07 +0200

    work in progress: building memorystore for vector models, and refactoring VectorContextSimilarity accordingly

--- index/src/main/scala/org/dbpedia/spotlight/db/MemoryVectorStoreIndexer.scala
+++ index/src/main/scala/org/dbpedia/spotlight/db/MemoryVectorStoreIndexer.scala
@@ -3,7 +3,6 @@ package org.dbpedia.spotlight.db
 import java.io.File
 import breeze.linalg.DenseMatrix
 import org.dbpedia.spotlight.db.model.{ResourceStore, TokenTypeStore}
-import org.dbpedia.spotlight.exceptions.DBpediaResourceNotFoundException
 
 import scala.collection.immutable.Iterable
 import scala.io.Source
@@ -15,13 +14,12 @@ import org.dbpedia.spotlight.model.{TokenType, DBpediaResource, TokenOccurrenceI
 /**
  * Created by dowling on 09/07/15.
  */
-class MemoryVectorStoreIndexer(modelPath: File, dictPath: File){
+class MemoryVectorStoreIndexer(val baseDir: File, modelPath: String, dictPath: String){
   lazy val contextStore = new MemoryVectorStore()
 
   var dict: Map[String, Int] = null
 
   def loadVectorDict(tokenTypeStore: TokenTypeStore, resourceStore: ResourceStore) = {
-    println("Loading vector dictionary!")
     dict = Source.fromFile(dictPath, "UTF-8").getLines().map { line =>
       val contents = line.split("\t")
       (contents(0), contents(1).toInt)
@@ -29,46 +27,20 @@ class MemoryVectorStoreIndexer(modelPath: File, dictPath: File){
     var resources: collection.mutable.Map[Int, Int] = collection.mutable.HashMap[Int,Int]()
     var tokens: collection.mutable.Map[Int, Int] = collection.mutable.HashMap[Int,Int]()
     // TODO: error handling if we can't find the token or resource
-    var failedResources = 0
-    var succeededResources = 0
-    var failedTokens = 0
-    var succeededTokens = 0
     dict.foreach { case(key, value) =>
       if(key.startsWith("DBPEDIA_ID/")){
-        try {
-          val resource = resourceStore.getResourceByName(key.replace("DBPEDIA_ID/", ""))
-          resources += (resource.id -> value)
-          succeededResources += 1
-        } catch {
-          case e: DBpediaResourceNotFoundException=> {
-            failedResources += 1
-            if (failedResources % 1000 == 0){
-              println("Can't find resource: " + key.replace("DBPEDIA_ID/", ""))
-            }
-          }
-        }
+        val resource = resourceStore.getResourceByName(key.replace("DBPEDIA_ID/", ""))
+        resources += (resource.id -> value)
       }else{
         val token = tokenTypeStore.getTokenType(key)
-        if (token == TokenType.UNKNOWN){
-          failedTokens += 1
-          if (failedTokens % 1000 == 0){
-            println("Can't find token: " + key)
-          }
-        } else {
-          tokens += (token.id -> value)
-          succeededTokens += 1
-        }
+        tokens += (token.id -> value)
       }
     }
-    println("Failed on " + failedResources + " entities, succeeded on " + succeededResources)
-    println("Failed on " + failedTokens + " tokens, succeeded on " + succeededTokens)
     contextStore.resourceIdToVectorIndex = resources.toMap
     contextStore.tokenTypeIdToVectorIndex = tokens.toMap
-    println("Done loading dict.")
   }
 
-  def loadVectorsAndWriteToStore(outputFile:File) = {
-    println("Loading vectors..")
+  def loadVectorsAndWriteToStore() = {
     val matrixSource = Source.fromFile(modelPath)
     val lines = matrixSource.getLines()
     val rows = lines.next().substring(2).toInt
@@ -85,7 +57,7 @@ class MemoryVectorStoreIndexer(modelPath: File, dictPath: File){
     }
     matrixSource.close()
     println("Done, dumping..")
-    MemoryStore.dump(contextStore, outputFile)
+    MemoryStore.dump(contextStore, new File(baseDir, "vectors.mem"))
   }
 
 }
