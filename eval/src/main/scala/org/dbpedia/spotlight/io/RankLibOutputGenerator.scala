76559f45f6af7f3aa68140b9f4b6080af6f1cf90
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-07-22 15:49:10 +0200

    added RankLib training data generation as a side effect of evaluation

--- eval/src/main/scala/org/dbpedia/spotlight/io/RankLibOutputGenerator.scala
+++ eval/src/main/scala/org/dbpedia/spotlight/io/RankLibOutputGenerator.scala
@@ -8,8 +8,32 @@ import org.dbpedia.spotlight.model.DisambiguationResult
  * Created by dowling on 22/07/15.
  */
 class RankLibOutputGenerator(val output: PrintWriter) extends OutputGenerator{
+  var qid = 0
+
   override def write(result: DisambiguationResult): Unit = {
-    result.predictedOccurrences.map{key => key.feature()}
+    qid += 1
+    var rank = 0
+
+    // skip the sample if the correct entity is not among predictions
+    if (result.predictedOccurrences.map{occ => occ.resource}.contains(result.correctOccurrence.resource)) {
+      result.predictedOccurrences.foreach { occ =>
+
+        if (occ.resource == result.correctOccurrence.resource)
+          rank = 1
+        else
+          rank = 2
+
+        output.println("%s qid:%s 1:%s 2:%s 3%s".format(
+          rank,
+          qid,
+          occ.feature("P(s|e)"),
+          occ.feature("P(c|e)"),
+          occ.feature("P(e)")
+        ))
+
+      }
+    }
+
   }
 
   override def summary(summaryString: String) {
