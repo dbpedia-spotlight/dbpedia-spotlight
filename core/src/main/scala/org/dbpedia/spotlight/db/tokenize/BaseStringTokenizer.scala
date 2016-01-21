ecf53c4afe293a9263a86376c63c056b352dd837
Jo Daiber <jodaiber@gmx.de>
2016-01-21 20:34:15 +0100

    Better whitespace check

--- core/src/main/scala/org/dbpedia/spotlight/db/tokenize/BaseStringTokenizer.scala
+++ core/src/main/scala/org/dbpedia/spotlight/db/tokenize/BaseStringTokenizer.scala
@@ -6,7 +6,7 @@ import org.dbpedia.spotlight.model.Text
 abstract class BaseStringTokenizer(stemmer: Stemmer) extends StringTokenizer {
 
   protected def tokenizeUnstemmed(text: String): Seq[String]
-  def tokenize(text: String): Seq[String] = tokenizeUnstemmed(text).map( stemmer.stem(_) ).filter(t => !t.isEmpty && !t.contains('\n'))
+  def tokenize(text: String): Seq[String] = tokenizeUnstemmed(text).map( stemmer.stem(_) ).filter(t => !t.isEmpty && !t.matches(".*\\s+.*"))
   def tokenize(text: Text): Seq[String] = tokenize(text.text)
 
 }
